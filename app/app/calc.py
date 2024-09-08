ab9fc63799fac401840183f91ca7f29d687042fa
messadki <mohamed.essadki.ecp@gmail.com>
2024-09-08 20:59:29 +0200

    wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Dummy Calculator functions
+"""
+
+
+def add(x, y):
+    """ add two numbers """
+    return x+y
+
+
+def substract(x, y):
+    """Subtract x from y"""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """Test adding two numbers"""
+
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_substract_numbers(self):
+        """Test substracting number"""
+
+        res = calc.substract(15, 10)
+        self.assertEqual(res, 5)
